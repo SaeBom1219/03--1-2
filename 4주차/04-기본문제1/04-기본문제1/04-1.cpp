ae645eaead1727bcbf19f9b38e519454a77cc5a2
SaeBom1219 <snkid0530@gmail.com>
2026-05-27 01:38:12 +0900

    Create 04-1.cpp
    04-기본문제1 풀이
    GPT와 같이 품

---
+++ 4주차/04-기본문제1/04-기본문제1/04-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+
+int main() {
+
+	int n[100];
+	int m[100];
+
+	cin >> m;
+
+
+	struct Info {
+		int max;
+		int min;
+		int sum;
+
+
+
+	}
+
+
+
+
+
+}
